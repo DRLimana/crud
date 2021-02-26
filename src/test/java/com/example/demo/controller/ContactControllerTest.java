236616be0e6592681dd9db51aa91f94277f316d9
drlimana <daniellimana@gmail.com>
2021-02-26 16:17:12 -0300

    second commit

---
+++ src/test/java/com/example/demo/controller/ContactControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(MockitoExtension.class)
+public class ContactControllerTest {
+	
+	private static final String CONTACT_URL_PATH= "/contacts";
+	
+	private MockMvc mockMvc;
+	
+	
+
+}
